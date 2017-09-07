87655aa068ded8517752ef432cd6cd212340c44f
KlausProg <KlausProg@KLAUSPROG-PC>
2017-09-06 22:01:07 -0400

    Added UserProfile fields

--- FitHub/FitHub.Web/Domain/UserProfile.cs
+++ FitHub/FitHub.Web/Domain/UserProfile.cs
@@ -2,10 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+//using FitHub.Web.Domain.Enums;  Could do this if we left the namespace the other way due to folder structure
 
 namespace FitHub.Web.Domain
 {
     public class UserProfile
     {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public int Height { get; set; }
+        public Gender Gender { get; set; }
+        public int Age { get; set; }
+
     }
 }
